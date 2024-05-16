473360bcb174cadd7b2da2b0de2b7750ba6062e6
steelPiber <pyh5523@gmail.com>
2024-05-16 20:33:09 +0900

    Update rsjpg formula with correct SHA256

---
+++ homebrew-tap/Formula/rsjpg.rb
@@ -0,0 +1,18 @@
+class Rsjpg < Formula
+  desc "A tool to combine multiple images into a single image"
+  homepage "https://github.com/steelPiber/rsjpg"
+  url "https://github.com/steelPiber/rsjpg/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "84a661ccc6a39ece8884dc35bdda60be5ccde76d856487580c7641d562d980db  rsjpg-0.0.2.tar"
+  license "MIT"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "--release"
+    bin.install "target/release/rsjpg"
+  end
+
+  test do
+    system "#{bin}/rsjpg", "--version"
+  end
+end
